4efaa6d3c3c72c85dfc17f7743241f020ff5b122
your_github_username <mohamed.inzamam@gmail.com>
2022-01-23 17:00:25 +0000

    Final Changes

---
+++ final_project/machinetranslation/tests/tests.py
@@ -0,0 +1,22 @@
+import unittest
+
+from translator import english_to_french, french_to_english
+
+class TestStringMethodsOne(unittest.TestCase):
+    def test_null_e2f(self):
+        self.assertEqual(english_to_french(),"")
+    def test_e2f(self):
+        self.assertEqual(english_to_french('Hello'), "Bonjour")
+    def test_e2f_2(self):
+        self.assertEqual(english_to_french('How are you?'), 'Comment es-tu?')
+
+class TestStringMethodsTwo(unittest.TestCase):
+    def test_null_f2e(self):
+        self.assertEqual(french_to_english(),"")
+    def test_f2e(self):
+        self.assertEqual(french_to_english('Bonjour'),"Hello")
+    def test_f2e_2(self):
+        self.assertEqual(french_to_english('Comment es-tu?'),"How are you?")
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,39 @@
+'''
+Translator.py
+'''
+import os
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+from dotenv import load_dotenv
+load_dotenv()
+
+apikey = os.environ['apikey']
+url = os.environ['url']
+
+authenticator = IAMAuthenticator(apikey)
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator
+)
+language_translator.set_service_url(url)
+
+
+def english_to_french(english_text=None):
+    '''English to French Translator Function '''
+    if english_text is None:
+        return ""
+    french_text = language_translator.translate(
+        text=english_text,
+        model_id='en-fr').get_result()['translations'][0]['translation']
+    return french_text
+
+
+def french_to_english(french_text=None):
+    '''French to English Translator Function '''
+    if french_text is None:
+        return ""
+    english_text = language_translator.translate(
+        text=french_text,
+        model_id='fr-en').get_result()['translations'][0]['translation']
+    return english_text
