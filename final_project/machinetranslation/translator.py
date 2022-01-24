a92117a788a3a4d1d3afafba1cb243a23b2868e3
inzi95 <mohamed.inzamam@gmail.com>
2022-01-24 09:48:04 +0530

    Final Changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -19,20 +19,16 @@ language_translator = LanguageTranslatorV3(
 language_translator.set_service_url(url)
 
 
-def english_to_french(english_text=None):
+def english_to_french(english_text):
     '''English to French Translator Function '''
-    if english_text is None:
-        return ""
     french_text = language_translator.translate(
         text=english_text,
         model_id='en-fr').get_result()['translations'][0]['translation']
     return french_text
 
 
-def french_to_english(french_text=None):
+def french_to_english(french_text):
     '''French to English Translator Function '''
-    if french_text is None:
-        return ""
     english_text = language_translator.translate(
         text=french_text,
         model_id='fr-en').get_result()['translations'][0]['translation']
